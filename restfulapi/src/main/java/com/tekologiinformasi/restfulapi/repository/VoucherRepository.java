c34374632e9a439ccf01fb72a1feb276f705dd06
Afdila-Sandi <afdilasandi23@gmail.com>
2025-02-28 09:37:49 +0700

    praktek di labor

--- restfulapi/src/main/java/com/tekologiinformasi/restfulapi/repository/VoucherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tekologiinformasi.restfulapi.repository;
-
-import com.tekologiinformasi.restfulapi.model.Voucher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoucherRepository extends JpaRepository<Voucher, Long> {
-}
